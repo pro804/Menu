6e633dbd0f049440f64945a06aaf68633efa33e8
pro804 <petsoulis.appdev@gmail.com>
2025-09-08 14:12:31 +0300

    feat: extract unique categories with Set and add state - Use Set object to get unique categories from menu data - Add 'all' category to the beginning of categories array - Implement categories state variable in App component - Log categories to console for verification

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import menu from "./data";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
 
+const allCategories = ["all", ...new Set(menu.map((items) => items.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
   return (
     <main>
       <section className="menu">

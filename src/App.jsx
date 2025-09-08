33ebd9bb68bb15e6094d50d4129335d9664a0e38
pro804 <petsoulis.appdev@gmail.com>
2025-09-08 14:38:09 +0300

    feat: create Categories component and render filter buttons - Build Categories component to display category buttons - Map over categories prop to render each button dynamically - Integrate Categories component in App with categories state - Style button container with flex layout and spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import menu from "./data";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
+import Categories from "./components/Categories";
 
 const allCategories = ["all", ...new Set(menu.map((items) => items.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
-  console.log(categories);
+
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
